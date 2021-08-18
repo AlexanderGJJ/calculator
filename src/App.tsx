82a98fb11851b62a2901a697868c27d35982d40b
mikhotin <ad.mihotin@gmail.com>
2021-08-18 23:35:51 +0300

    init app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './st.module.css';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1 className={styles.test}>Calculator 1.0</h1>
+            </div>
+
+        );
+    }
+}
+
+export default App;
